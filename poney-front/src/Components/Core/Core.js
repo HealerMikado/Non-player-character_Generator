66919e8ef0bdde705f3ed349ec09a4701756bf59
Antoine Brunetti <antoine.brunetti@insee.fr>
2019-06-25 15:35:20 +0200

    componentisation

--- poney-front/src/Components/Core/Core.js
+++ poney-front/src/Components/Core/Core.js
@@ -2,61 +2,76 @@ import React from "react";
 import PropTypes from "prop-types";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/";
 import Fab from "@material-ui/core/Fab";
 import Add from "@material-ui/icons/Add";
+import FilterVintageRounded from "@material-ui/icons/FilterVintageRounded";
 import Search from "@material-ui/icons/Search";
 import InputBase from "@material-ui/core/InputBase";
 import Switch from "@material-ui/core/Switch";
 import Tooltip from "@material-ui/core/Tooltip";
 import PoneyGrid from "./PoneyGrid";
-export default function Core() {
-  const [state, setState] = React.useState({
-    checkedA: false
-  });
+import theme from "../../Theme";
+class Core extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedA: false
+    };
+  }
 
-  const handleChange = name => event => {
-    setState({ ...state, [name]: event.target.checked });
-  };
-
-  const classes = useStyles();
-  return (
-    <Container>
-      <Tooltip title="Activer la suppression">
-        <Switch
-          className={classes.Switch}
-          checked={state.checkedA}
-          onChange={handleChange("checkedA")}
-          value="checkedA"
-          label="Supprimer"
-          inputProps={{ "aria-label": "secondary checkbox" }}
-          labelPlacement="bottom"
-        />
-      </Tooltip>
-      <Paper className={classes.paper}>
-        <h1 className={classes.h1}>Ecurie</h1>
-        <Fab variant="extended" aria-label="Add" className={classes.fab}>
-          <Add className={classes.extendedIcon} />
-          &nbsp; Ajouter un nouveau poney
-        </Fab>
-        <Fab variant="extended" className={classes.fab2} disableRipple={true}>
-          <Search className={classes.extendedIcon} />
-          <InputBase
-            placeholder="Recherchez votre poney"
-            classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput
-            }}
-            inputProps={{ "aria-label": "Search" }}
+  handleSwitch() {
+    this.setState({ checkedA: !this.state.checkedA });
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container>
+        <Tooltip title="Activer la suppression">
+          <Switch
+            className={classes.Switch}
+            checked={this.state.checkedA}
+            onChange={this.handleChange}
+            value="checkedA"
+            label="Supprimer"
+            inputProps={{ "aria-label": "secondary checkbox" }}
+            labelPlacement="bottom"
           />
-        </Fab>
-        <PoneyGrid checked={true} />
-      </Paper>
-    </Container>
-  );
+        </Tooltip>
+        <Paper className={classes.paper}>
+          <h1 className={classes.h1}>Ecurie</h1>
+          <Fab variant="extended" aria-label="Add" className={classes.fab}>
+            <Add className={classes.extendedIcon} />
+            &nbsp; Ajouter un nouveau poney
+          </Fab>
+          <Tooltip title="Ajouter un nouveau poney random">
+            <Fab
+              variant="round"
+              aria-label="AddRandom"
+              className={classes.fab3}
+            >
+              <FilterVintageRounded className={classes.extendedIcon} />
+            </Fab>
+          </Tooltip>
+          <Fab variant="extended" className={classes.fab2} disableRipple={true}>
+            <Search className={classes.extendedIcon} />
+            <InputBase
+              placeholder="Recherchez votre poney"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "Search" }}
+            />
+          </Fab>
+          <PoneyGrid checked={true} />
+        </Paper>
+      </Container>
+    );
+  }
 }
 
-const useStyles = makeStyles(theme => ({
+const styles = {
   h1: {
     color: "black",
     fontFamily: "Cormorant Garamond",
@@ -91,6 +106,17 @@ const useStyles = makeStyles(theme => ({
     padding: "0 30px",
     float: "right"
   },
+  fab3: {
+    marginBottom: "57px",
+    marginLeft: 200,
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "black",
+    height: 48,
+    padding: "0 30px"
+  },
   inputRoot: {
     color: "inherit"
   },
@@ -105,10 +131,8 @@ const useStyles = makeStyles(theme => ({
       }
     }
   }
-}));
-
-Core.propTypes = {
-  classes: PropTypes.object.isRequired
 };
+
+export default withStyles(styles)(Core);
 
 //https://www.pixelstalk.net/wp-content/uploads/2016/04/MLP-wallpapers-HD-cartoon-fantasy-horses-horse-unicorn.png
