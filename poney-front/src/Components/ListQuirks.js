181e713760e54eaaa7d82eec7045938a755579e1
Rémi Pépin <remi.pepin@insee.fr>
2019-06-26 15:30:09 +0200

    delete useless import

--- poney-front/src/Components/ListQuirks.js
+++ poney-front/src/Components/ListQuirks.js
@@ -5,9 +5,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
 
 class ListQuirks extends React.Component {
