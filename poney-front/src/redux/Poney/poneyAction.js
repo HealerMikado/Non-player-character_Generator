eceda01dde912eb9cc25930a9fb1132081750444
Antoine Brunetti <antoine.brunetti@insee.fr>
2019-06-28 16:05:00 +0200

    fine

--- poney-front/src/redux/Poney/poneyAction.js
+++ poney-front/src/redux/Poney/poneyAction.js
@@ -47,23 +47,21 @@ export const fetchPonyById = name => {
 
 export const postPony = pony => {
   return (dispatch, getState) => {
-    fetch(
-      "http://localhost:8080/creation",
-      this.state.pony,
-      {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json"
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        body: JSON.stringify(setPony)
-      }.then(response => response.json())
-    );
+    fetch("http://localhost:8080/generate/creation", getState.pony, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(pony)
+    }).then(result => {
+      dispatch(setPony(result));
+    });
   };
 };
 
